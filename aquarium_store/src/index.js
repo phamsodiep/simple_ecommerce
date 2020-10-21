f957f4db506b3c6b2536a118abae449896c6d2e3
phamsodiep <phamduchuy@hotmail.com>
2020-10-21 09:37:11 +0700

    [aquarium_store] Initial creating project aquarium_store

---
+++ aquarium_store/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+ReactDOM.render(
+  <React.Fragment>
+      <h1>Cá Kiểng Phong Thủy</h1>
+      <p>Trang web đang được xây dựng</p>
+      <p>© cakiengphongthuy.blogspot.com</p>
+  </React.Fragment>,
+  document.getElementById('aquarium_store')
+);
+
